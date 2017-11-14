06ca1f9eadbd10021f3382a1fe4de77a9e90d796
qml95 <quentin.morino.laudrin@gmail.com>
2017-11-14 10:45:51 +0100

    finalisation de la fonction ajaxget pour afficher douze profil random

--- js/public_api.js
+++ js/public_api.js
@@ -2,30 +2,30 @@
 var employeElt = document.getElementById('employe');
 
 
-//utilistion de la fonction AJAX en Appelle
+
+//utilistion de la fonction ajaxGet pour afficher les douze profils
 ajaxGet("https://randomuser.me/api/?results=12", function(reponse) {
   var profil = JSON.parse(reponse);
-  console.log(profil);
-  var profilElt = document.createElement('div');
-  profilElt.setAttribute("id", "profil")
-  profilElt.innerHTML =`
-    <img src="${profil.results[0].picture.medium}">
-    <p id="description">
-    <p id="info">
-      ${profil.results[0].name.title}
-      ${profil.results[0].name.first}
-      ${profil.results[0].name.last}
-    </p>
-    <p class="infosecondaire">
-      ${profil.results[0].email}
-    </p>
-    <p class="infosecondaire">
-      ${profil.results[0].location.city}
-    </p>
-    <p>
-  `;
-
-
-  console.log(profilElt);
-  employeElt.appendChild(profilElt);
+  var mesProfils = profil.results;
+  for (var i = 0; i < mesProfils.length; i++) {
+    var profilElt = document.createElement('div');
+    profilElt.setAttribute("id", "profil")
+    profilElt.innerHTML =`
+        <img src="${mesProfils[i].picture.medium}">
+        <p id="description">
+        <p id="info">
+          ${mesProfils[i].name.title}
+          ${mesProfils[i].name.first}
+          ${mesProfils[i].name.last}
+        </p>
+        <p class="infosecondaire">
+          ${mesProfils[i].email}
+        </p>
+        <p class="infosecondaire">
+          ${mesProfils[i].location.city}
+        </p>
+        <p>
+      `;
+employeElt.appendChild(profilElt);
+  }
 });
