76bdea551447c41d8dcd8dfe968b4d7df49ad40f
qml95 <quentin.morino.laudrin@gmail.com>
2017-11-14 15:21:29 +0100

    ajout du css pour la lightbox; ajout d'une fonction overlay

--- js/public_api.js
+++ js/public_api.js
@@ -9,23 +9,54 @@ ajaxGet("https://randomuser.me/api/?results=12", function(reponse) {
     var profilElt = document.createElement('div');
     profilElt.setAttribute("id", "profil")
     profilElt.innerHTML =`
-        <img data-img="{mesProfils[i].picture.medium}" src="${mesProfils[i].picture.medium}">
-        <p id="description">
-        <p id="info">
-          ${mesProfils[i].name.title}
-          ${mesProfils[i].name.first}
-          ${mesProfils[i].name.last}
-        </p>
-        <p class="infosecondaire">
-          ${mesProfils[i].email}
-        </p>
-        <p class="infosecondaire">
-          ${mesProfils[i].location.city}
-        </p>
-        <p>
+      <div onclick=caca(this)>
+        <img data-img="{mesProfils[i].picture.medium}" src="${mesProfils[i].picture.medium}" id="img">
+        <div id="description">
+          <p id="info">
+            ${mesProfils[i].name.title}
+            ${mesProfils[i].name.first}
+            ${mesProfils[i].name.last}
+          </p>
+          <p class="infosecondaire">
+            ${mesProfils[i].email}
+          </p>
+          <p class="infosecondaire">
+            ${mesProfils[i].location.city}
+          </p>
+        <div>
+      </div>
       `;
 employeElt.appendChild(profilElt);
   }
 });
 
 //partie de lightbox
+var body = document.querySelector('body');
+function caca(employeHtml) {
+  var essais = employeHtml;
+  essais.setAttribute('class', 'overlaytitle');
+
+  //création d'un élément
+  var lightboxElt = document.createElement('div');
+  lightboxElt.setAttribute('id', 'overlay');
+body.appendChild(lightboxElt);
+lightboxElt.appendChild(essais);
+
+  //ajout de la lightbox au body
+
+  lightboxElt.addEventListener('click', function(){
+
+      lightboxElt.remove();
+  
+  });
+
+
+}
+
+// function lightbox() {
+//
+// }
+//
+// document.getElementById('img').addEventListener('click', function(){
+//   lightbox();
+// });
